47bc8ccaa96aad3445f6512bb6da5cc11e36e867
Chelamkuri HemaDurga <chelamkurihemadurga@gmail.com>
2026-07-25 13:04:04 +0530

    Connect frontend with deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:5000"
+    baseURL: "https://leaddesk-backend-4lhk.onrender.com"
+   
 });
 export const deleteLead = (id) => {
 
